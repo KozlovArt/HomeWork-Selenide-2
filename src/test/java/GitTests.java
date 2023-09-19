b52d31e3be48fe8e4f81405c800ca4cd4e195c9e
KozlovArt <144378790+KozlovArt@users.noreply.github.com>
2023-09-19 20:56:53 +0300

    addit

--- src/test/java/GitTests.java
+++ src/test/java/GitTests.java
@@ -24,8 +24,11 @@ public class GitTests {
     @Test
     void dragAndDropTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         $("#column-a").dragAndDropTo($("#column-b"));
         $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
 
     }
 }
